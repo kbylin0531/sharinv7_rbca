13a31dd38b50c48859a98092185cfe81e9303e6b
linzhv <linzhv@qq.com>
2016-10-10 12:07:09 +0800

    storage done

--- Sharin/Test/Core/storage.php
+++ Sharin/Test/Core/storage.php
@@ -23,29 +23,67 @@ namespace {
         '/home/asus/workspace/Sharingan/README.md',
         '/home/asus/workspace/Sharingan/',
     ];
+//    $result['has'] = [];
+//    foreach ($files4readabletest as $file) {
+//        $result['has'][] = Storage::has($file);
+//    }
+//
+//    $result['size'] = [];
+//    foreach ($files4readabletest as $file) {
+//        $result['size'][] = Storage::size($file);
+//    }
+//
+//    $result['mtime'] = [];
+//    foreach ($files4readabletest as $file) {
+//        $result['mtime'][] = Storage::mtime($file);
+//    }
+//
+//    $result['read'] = [];
+//    foreach ($files4readabletest as $file) {
+//        $result['read'][] = Storage::read($file);
+//    }
+//
+//    $result['readdir'] = [];
+//    foreach ($files4readabletest as $file) {
+//        $result['readdir'][] = Storage::readdir($file,true);
+//    }
 
-    //test has
-    $result['has'] = [];
-    foreach ($files4readabletest as $file) {
-        $result['has'][] = Storage::has($file);
+
+    $files4writetest = [
+        SR_PATH_FRAMEWORK.'/Test/runtime',//在受保护的范围内
+        SR_PATH_FRAMEWORK.'/Test/runtime/a.cache',//在受保护的范围内
+
+        SR_PATH_BASE.'/Runtime',
+        SR_PATH_BASE.'/Runtime/test',
+        SR_PATH_BASE.'/Runtime/testdir',
+        SR_PATH_BASE.'/Runtime/testdir2',
+
+    ];
+//    $result['mkdir'] = [];
+//    foreach ($files4writetest as $file) {
+//        $result['mkdir'][] = Storage::mkdir($file,0777);
+//    }
+//    $result['chmod'] = [];
+//    foreach ($files4writetest as $file) {
+//        $result['chmod'][] = Storage::chmod($file,0751);
+//    }
+//    $result['touch'] = [];
+//    foreach ($files4writetest as $file) {
+//        $result['touch'][] = Storage::touch($file,0,0);
+//    }
+//    $result['write'] = [];
+//    foreach ($files4writetest as $file) {
+//        $result['write'][] = Storage::write($file,"hello");
+//    }
+//    $result['append'] = [];
+//    foreach ($files4writetest as $file) {
+//        $result['append'][] = Storage::append($file,"hello");
+//    }
+    $result['rmdir'] = [];
+    foreach ($files4writetest as $file) {
+        $result['rmdir'][] = Storage::unlink($file,true);
     }
 
-    //test size
-    $result['size'] = [];
-    foreach ($files4readabletest as $file) {
-        $result['size'][] = Storage::size($file);
-    }
-
-    //test mtime
-    $result['mtime'] = [];
-    foreach ($files4readabletest as $file) {
-        $result['mtime'][] = Storage::mtime($file);
-    }
-
-    $result['read'] = [];
-    foreach ($files4readabletest as $file) {
-        $result['read'][] = Storage::read($file);
-    }
 
 
     \Sharin\dumpout($result);
